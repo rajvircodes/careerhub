db4cac78fb7509bbc5e55333ffd2a21df48a68ed
rajvircodes <rajvirsinhgohil004@gmail.com>
2026-07-01 17:15:52 +0530

    fixt: cookie parser invoked properly

--- backend/src/app.js
+++ backend/src/app.js
@@ -15,7 +15,7 @@ app.use(morgan("dev"));
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cookieParser);
+app.use(cookieParser());
 
 // =============== Health Route ===================
 
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,5 +1,6 @@
 const app = require("./app");
 const { PORT, NODE_ENV } = require("./config/env");
+
 app.listen(PORT, () => {
   console.log(`Server is running in ${NODE_ENV} on ${PORT}`);
 });
